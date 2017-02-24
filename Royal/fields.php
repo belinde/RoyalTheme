6ff3a48adebb7ab0c330f2c84f04bf0eff6faa32
Franco Traversaro <franco.traversaro@wyscout.com>
2017-02-24 18:28:25 +0100

    Modifiche

--- Royal/fields.php
+++ Royal/fields.php
@@ -4,76 +4,90 @@ namespace Royal\Fields;
 use Royal\Fields\AbstractField as _;
 
 return [
-	( new Boolean( 'evidenza', "In evidenza" ) )
-		->setInternal()
-	,
-	( new Select( 'status' ) )
-		->setValues( [
-			'disponibile' => 'disponibile sul mercato',
-			'trattativa'  => 'in trattativa',
-			'terminato'   => 'venduto o piazzato'
-		] )
-		->setHelp( "Determina se l'annuncio è visibile o meno sul sito" )
-		->setInternal()
-	,
-	( new Text( 'proprietario' ) )
-		->setInternal()
-	,
-	( new Integer( 'prezzo' ) )
-		->setAppend( '€' )
-		->setEmpty( 'trattativa riservata' )
-		->setSearch( _::SEARCH_RANGE )
-		->setInternal()
-	,
-	( new Integer( 'vani' ) )
-		->setSearch( _::SEARCH_RANGE )
-	,
-	( new Integer( 'superficie' ) )
-		->setAppend( 'm&sup2;' )
-		->setSearch( _::SEARCH_RANGE )
-	,
-	( new Select( 'condizione' ) )
-		->setValues( [
-			'da_ristrutturare' => 'da ristrutturare',
-			'ristrutturato'    => 'ristrutturato',
-			'buono'            => 'buono',
-			'nuovo'            => 'nuovo'
-		] )
-	,
-	( new Integer( 'bagni', "Numero bagni" ) )
-		->setSearch( _::SEARCH_RANGE )
-	,
-	( new Integer( 'balconi' ) )
-	,
-	( new Integer( 'terrazzi' ) )
-	,
-	( new Select( 'riscaldamento' ) )
-		->setValues( [
-			'assente'      => 'assente',
-			'condominio'   => 'condominiale',
-			'indipendente' => 'termoautonomo'
-		] )
-		->setSearch( _::SEARCH_RANGE )
-	,
-	( new Select( 'condizionatore', "Aria condizionata" ) )
-		->setValues( [
-			'assente'     => 'assente',
-			'predisposto' => 'con predisposizione',
-			'presente'    => 'presente'
-		] )
-		->setSearch( _::SEARCH_EXACT )
-	,
-	( new Boolean( 'ascensore' ) )
-		->setSearch( _::SEARCH_EXACT )
-	,
-	( new Boolean( 'box', "Box auto" ) )
-		->setSearch( _::SEARCH_EXACT )
-	,
-	( new Select( 'posteggio', "Posto auto" ) )
-		->setValues( [
-			'coperto'  => 'coperto',
-			'scoperto' => 'scoperto'
-		] )
-	,
-	( new LongText( 'indirizzo', "Indirizzo completo" ) )
+    (new Boolean('evidenza', "In evidenza"))
+        ->setInternal()
+    ,
+    (new Select('status'))
+        ->setValues([
+            'disponibile' => 'disponibile sul mercato',
+            'trattativa'  => 'in trattativa',
+            'terminato'   => 'venduto o piazzato'
+        ])
+        ->setHelp("Determina se l'annuncio è visibile o meno sul sito")
+        ->setInternal()
+    ,
+    (new Text('proprietario'))
+        ->setInternal()
+    ,
+    (new Integer('prezzo'))
+        ->setAppend('€')
+        ->setEmpty('trattativa riservata')
+        ->setSearch(_::SEARCH_RANGE)
+        ->setInternal()
+    ,
+    (new Integer('vani'))
+        ->setSearch(_::SEARCH_RANGE)
+    ,
+    (new Integer('superficie'))
+        ->setAppend('m&sup2;')
+        ->setSearch(_::SEARCH_RANGE)
+    ,
+    (new Select('condizione'))
+        ->setValues([
+            'da_ristrutturare' => 'da ristrutturare',
+            'ristrutturato'    => 'ristrutturato',
+            'buono'            => 'buono',
+            'nuovo'            => 'nuovo'
+        ])
+    ,
+    (new Integer('bagni', "Numero bagni"))
+        ->setSearch(_::SEARCH_RANGE)
+    ,
+    (new Integer('balconi'))
+    ,
+    (new Integer('terrazzi'))
+    ,
+    (new Select('riscaldamento'))
+        ->setValues([
+            'assente'      => 'assente',
+            'condominio'   => 'condominiale',
+            'indipendente' => 'termoautonomo'
+        ])
+        ->setSearch(_::SEARCH_RANGE)
+    ,
+    (new Select('condizionatore', "Aria condizionata"))
+        ->setValues([
+            'assente'     => 'assente',
+            'predisposto' => 'con predisposizione',
+            'presente'    => 'presente'
+        ])
+        ->setSearch(_::SEARCH_EXACT)
+    ,
+    (new Boolean('ascensore'))
+        ->setSearch(_::SEARCH_EXACT)
+    ,
+    (new Boolean('box', "Box auto"))
+        ->setSearch(_::SEARCH_EXACT)
+    ,
+    (new Select('posteggio', "Posto auto"))
+        ->setValues([
+            'coperto'  => 'coperto',
+            'scoperto' => 'scoperto'
+        ])
+    ,
+    (new Select('tipoaffitto', "Tipo di affitto"))
+        ->setValues([
+            'residenziale' => 'residenziale',
+            'turistico'    => 'turistico'
+        ])
+        ->setSearch(_::SEARCH_EXACT)
+    ,
+    (new Select('arredo', "Arredo"))
+        ->setValues([
+            'ammobiliato'    => 'ammobiliato',
+            'nonammobiliato' => 'non ammobiliato'
+        ])
+        ->setSearch(_::SEARCH_EXACT)
+    ,
+    (new LongText('indirizzo', "Indirizzo completo"))
 ];
